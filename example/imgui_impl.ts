d3a27fce1a457ad715cfbb37c3b1aafb14beda97
Isaac Burns <isaacburns@gmail.com>
2018-02-21 13:29:53 -0600

    add some simple gamepad detection

--- example/imgui_impl.ts
+++ example/imgui_impl.ts
@@ -318,57 +318,87 @@ export function NewFrame(time: number): void {
                 if (io.NavInputs[NAV_NO] < v) io.NavInputs[NAV_NO] = v;
             }
             // TODO: map input based on vendor and product id
-            // id: Logitech Logitech Dual Action (Vendor: 046d Product: c216)
-            // MAP_BUTTON(ImGuiNavInput.Activate,    1); // Cross / A
-            // MAP_BUTTON(ImGuiNavInput.Cancel,      2); // Circle / B
-            // MAP_BUTTON(ImGuiNavInput.Menu,        0); // Square / X
-            // MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
-            // MAP_ANALOG(ImGuiNavInput.DpadLeft,    4, -0.3, -0.9); // D-Pad Left
-            // MAP_ANALOG(ImGuiNavInput.DpadRight,   4, +0.3, +0.9); // D-Pad Right
-            // MAP_ANALOG(ImGuiNavInput.DpadUp,      5, -0.3, -0.9); // D-Pad Up
-            // MAP_ANALOG(ImGuiNavInput.DpadDown,    5, +0.3, +0.9); // D-Pad Down
-            // MAP_BUTTON(ImGuiNavInput.FocusPrev,   4); // L1 / LB
-            // MAP_BUTTON(ImGuiNavInput.FocusNext,   5); // R1 / RB
-            // MAP_BUTTON(ImGuiNavInput.TweakSlow,   6); // L2 / LT
-            // MAP_BUTTON(ImGuiNavInput.TweakFast,   7); // R2 / RT
-            // MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
-            // MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
-            // MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
-            // MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
-            // id: Logitech Gamepad F310 (STANDARD GAMEPAD Vendor: 046d Product: c21d)
-            MAP_BUTTON(ImGuiNavInput.Activate,    0); // Cross / A
-            MAP_BUTTON(ImGuiNavInput.Cancel,      1); // Circle / B
-            MAP_BUTTON(ImGuiNavInput.Menu,        2); // Square / X
-            MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
-            MAP_BUTTON(ImGuiNavInput.DpadLeft,    14); // D-Pad Left
-            MAP_BUTTON(ImGuiNavInput.DpadRight,   15); // D-Pad Right
-            MAP_BUTTON(ImGuiNavInput.DpadUp,      12); // D-Pad Up
-            MAP_BUTTON(ImGuiNavInput.DpadDown,    13); // D-Pad Down
-            MAP_BUTTON(ImGuiNavInput.FocusPrev,   4); // L1 / LB
-            MAP_BUTTON(ImGuiNavInput.FocusNext,   5); // R1 / RB
-            MAP_ANALOG(ImGuiNavInput.TweakSlow,   6, +0.3, +0.9); // L2 / LT
-            MAP_ANALOG(ImGuiNavInput.TweakFast,   7, +0.3, +0.9); // R2 / RT
-            MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
-            MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
-            MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
-            MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
-            // id: 8Bitdo SN30 Pro  8Bitdo SN30 Pro (Vendor: 2dc8 Product: 6001)
-            // MAP_BUTTON(ImGuiNavInput.Activate,    1); // Cross / A
-            // MAP_BUTTON(ImGuiNavInput.Cancel,      0); // Circle / B
-            // MAP_BUTTON(ImGuiNavInput.Menu,        4); // Square / X
-            // MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
-            // MAP_ANALOG(ImGuiNavInput.DpadLeft,    6, -0.3, -0.9); // D-Pad Left
-            // MAP_ANALOG(ImGuiNavInput.DpadRight,   6, +0.3, +0.9); // D-Pad Right
-            // MAP_ANALOG(ImGuiNavInput.DpadUp,      7, -0.3, -0.9); // D-Pad Up
-            // MAP_ANALOG(ImGuiNavInput.DpadDown,    7, +0.3, +0.9); // D-Pad Down
-            // MAP_BUTTON(ImGuiNavInput.FocusPrev,   6); // L1 / LB
-            // MAP_BUTTON(ImGuiNavInput.FocusNext,   7); // R1 / RB
-            // MAP_BUTTON(ImGuiNavInput.TweakSlow,   8); // L2 / LT
-            // MAP_BUTTON(ImGuiNavInput.TweakFast,   9); // R2 / RT
-            // MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
-            // MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
-            // MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
-            // MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
+            const match: RegExpMatchArray | null = gamepad.id.match(/^.*\(.*Vendor: ([0-9a-f]{4}) Product: ([0-9a-f]{4})\).*/);
+            const vendor: string = match && match[1] || "0000";
+            const product: string = match && match[2] || "0000";
+            switch (vendor + product) {
+                case "046dc216":
+                // id: Logitech Logitech Dual Action (Vendor: 046d Product: c216)
+                MAP_BUTTON(ImGuiNavInput.Activate,    1); // Cross / A
+                MAP_BUTTON(ImGuiNavInput.Cancel,      2); // Circle / B
+                MAP_BUTTON(ImGuiNavInput.Menu,        0); // Square / X
+                MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
+                MAP_ANALOG(ImGuiNavInput.DpadLeft,    4, -0.3, -0.9); // D-Pad Left
+                MAP_ANALOG(ImGuiNavInput.DpadRight,   4, +0.3, +0.9); // D-Pad Right
+                MAP_ANALOG(ImGuiNavInput.DpadUp,      5, -0.3, -0.9); // D-Pad Up
+                MAP_ANALOG(ImGuiNavInput.DpadDown,    5, +0.3, +0.9); // D-Pad Down
+                MAP_BUTTON(ImGuiNavInput.FocusPrev,   4); // L1 / LB
+                MAP_BUTTON(ImGuiNavInput.FocusNext,   5); // R1 / RB
+                MAP_BUTTON(ImGuiNavInput.TweakSlow,   6); // L2 / LT
+                MAP_BUTTON(ImGuiNavInput.TweakFast,   7); // R2 / RT
+                MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
+                break;
+                case "046dc21d":
+                // id: Logitech Gamepad F310 (STANDARD GAMEPAD Vendor: 046d Product: c21d)
+                MAP_BUTTON(ImGuiNavInput.Activate,    0); // Cross / A
+                MAP_BUTTON(ImGuiNavInput.Cancel,      1); // Circle / B
+                MAP_BUTTON(ImGuiNavInput.Menu,        2); // Square / X
+                MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
+                MAP_BUTTON(ImGuiNavInput.DpadLeft,    14); // D-Pad Left
+                MAP_BUTTON(ImGuiNavInput.DpadRight,   15); // D-Pad Right
+                MAP_BUTTON(ImGuiNavInput.DpadUp,      12); // D-Pad Up
+                MAP_BUTTON(ImGuiNavInput.DpadDown,    13); // D-Pad Down
+                MAP_BUTTON(ImGuiNavInput.FocusPrev,   4); // L1 / LB
+                MAP_BUTTON(ImGuiNavInput.FocusNext,   5); // R1 / RB
+                MAP_ANALOG(ImGuiNavInput.TweakSlow,   6, +0.3, +0.9); // L2 / LT
+                MAP_ANALOG(ImGuiNavInput.TweakFast,   7, +0.3, +0.9); // R2 / RT
+                MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
+                break;
+                case "2dc86001":
+                // id: 8Bitdo SN30 Pro  8Bitdo SN30 Pro (Vendor: 2dc8 Product: 6001)
+                MAP_BUTTON(ImGuiNavInput.Activate,    1); // Cross / A
+                MAP_BUTTON(ImGuiNavInput.Cancel,      0); // Circle / B
+                MAP_BUTTON(ImGuiNavInput.Menu,        4); // Square / X
+                MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
+                MAP_ANALOG(ImGuiNavInput.DpadLeft,    6, -0.3, -0.9); // D-Pad Left
+                MAP_ANALOG(ImGuiNavInput.DpadRight,   6, +0.3, +0.9); // D-Pad Right
+                MAP_ANALOG(ImGuiNavInput.DpadUp,      7, -0.3, -0.9); // D-Pad Up
+                MAP_ANALOG(ImGuiNavInput.DpadDown,    7, +0.3, +0.9); // D-Pad Down
+                MAP_BUTTON(ImGuiNavInput.FocusPrev,   6); // L1 / LB
+                MAP_BUTTON(ImGuiNavInput.FocusNext,   7); // R1 / RB
+                MAP_BUTTON(ImGuiNavInput.TweakSlow,   8); // L2 / LT
+                MAP_BUTTON(ImGuiNavInput.TweakFast,   9); // R2 / RT
+                MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
+                break;
+                default:
+                // standard gamepad: https://w3c.github.io/gamepad/#remapping
+                MAP_BUTTON(ImGuiNavInput.Activate,    0); // Cross / A
+                MAP_BUTTON(ImGuiNavInput.Cancel,      1); // Circle / B
+                MAP_BUTTON(ImGuiNavInput.Menu,        2); // Square / X
+                MAP_BUTTON(ImGuiNavInput.Input,       3); // Triangle / Y
+                MAP_BUTTON(ImGuiNavInput.DpadLeft,    14); // D-Pad Left
+                MAP_BUTTON(ImGuiNavInput.DpadRight,   15); // D-Pad Right
+                MAP_BUTTON(ImGuiNavInput.DpadUp,      12); // D-Pad Up
+                MAP_BUTTON(ImGuiNavInput.DpadDown,    13); // D-Pad Down
+                MAP_BUTTON(ImGuiNavInput.FocusPrev,   4); // L1 / LB
+                MAP_BUTTON(ImGuiNavInput.FocusNext,   5); // R1 / RB
+                MAP_BUTTON(ImGuiNavInput.TweakSlow,   6); // L2 / LT
+                MAP_BUTTON(ImGuiNavInput.TweakFast,   7); // R2 / RT
+                MAP_ANALOG(ImGuiNavInput.LStickLeft,  0, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickRight, 0, +0.3, +0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickUp,    1, -0.3, -0.9);
+                MAP_ANALOG(ImGuiNavInput.LStickDown,  1, +0.3, +0.9);
+                break;
+            }
         }
     }
 
