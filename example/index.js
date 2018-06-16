ef0f46c9cf9642734da44a2b89ad42838b66b931
Isaac Burns <isaacburns@gmail.com>
2018-06-16 10:26:00 -0500

    add fetch polyfill for node example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,24 @@
 /// @ts-check
 /// <reference types="node"/>
 
+const fs = require('fs');
+const Response = require('node-fetch').Response;
+
+global["fetch"] = (url) => {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(url)) {
+      reject(`File not found: ${url}`);
+    }
+    const readStream = fs.createReadStream(url);
+    readStream.on('open', function () {
+      resolve(new Response(readStream, {
+        status: 200,
+        statusText: 'OK'
+      }));
+    });
+  });
+};
+
 process.chdir(__dirname);
 
 global["SystemJS"] = require("systemjs");
