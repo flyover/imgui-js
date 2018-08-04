1e5500d15a4bbe3edcf4cae120e309256a5ffa44
Isaac Burns <isaacburns@gmail.com>
2018-08-04 12:27:55 -0500

    first pass at rollup dist

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import commonjs from 'rollup-plugin-commonjs';
+import builtins from "rollup-plugin-node-builtins";
+import typescript from "rollup-plugin-typescript2";
+
+const plugins = [
+  typescript({
+    clean: true,
+    tsconfigOverride: {
+      compilerOptions: {
+        target: "ES2015",
+        module: "ES2015"
+      }
+    }
+  }),
+  commonjs({
+    namedExports: {
+      "bind-imgui.js": [ "bind" ]
+    }
+  }),
+  builtins(),
+];
+
+export default [
+  {
+    input: "imgui.ts",
+    output: {
+      file: "dist/imgui.umd.js",
+      name: "ImGui",
+      format: "umd",
+      exports: "named"
+    },
+    plugins: plugins
+  },
+  {
+    input: "example/imgui_impl.ts",
+    output: {
+      file: "dist/imgui_impl.umd.js",
+      name: "ImGui_Impl",
+      format: "umd",
+      exports: "named"
+    },
+    plugins: plugins
+  }
+];
