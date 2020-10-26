ba0df99f82d89b721d67dc4d93f05545522c1858
Tomáš Vojtášek <vojtasek.tomas@seznam.cz>
2020-10-26 13:49:41 +0100

    Fixed nodejs example

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
 /// @ts-check
 /// <reference types="node"/>
 
-const fs = require('fs');
-const path = require('path');
-const Response = require('node-fetch').Response;
+const fs = require("fs");
+const path = require("path");
+const Response = require("node-fetch").Response;
 
 global["__filename"] = module.filename;
 global["__dirname"] = path.dirname(module.filename);
@@ -14,21 +14,17 @@ global["fetch"] = (url) => {
       reject(`File not found: ${url}`);
     }
     const readStream = fs.createReadStream(url);
-    readStream.on('open', function () {
-      resolve(new Response(readStream, {
-        status: 200,
-        statusText: 'OK'
-      }));
+    readStream.on("open", function () {
+      resolve(
+        new Response(readStream, {
+          status: 200,
+          statusText: "OK",
+        })
+      );
     });
   });
 };
 
 process.chdir(__dirname);
 
-global["SystemJS"] = require("systemjs");
-
-module.require("./system.config");
-
-SystemJS.import("./main")
-.then(function (main) { main.default(); })
-.catch(console.error);
+require("./main");
